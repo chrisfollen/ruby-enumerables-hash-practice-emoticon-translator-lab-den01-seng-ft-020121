1636d77c2db7ede571ddd75364ef1b748b5c1f77
Chris Follen <christopher.follen@gmail.com>
2021-01-11 20:44:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,6 +23,7 @@ def get_japanese_emoticon(path, emot_in)
       return value[:japanese]
     end
   end
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, emot_in)
@@ -36,6 +37,4 @@ def get_english_meaning(path, emot_in)
     end
   end
   return "Sorry, that emoticon was not found"
-  
-  
 end
