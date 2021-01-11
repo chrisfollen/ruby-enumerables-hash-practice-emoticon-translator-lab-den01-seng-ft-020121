c43af5e852de48c183341a61556cb3c874a8b9b7
Chris Follen <christopher.follen@gmail.com>
2021-01-11 20:14:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,19 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
+def load_library(path)
   # code goes here
+  library = {}
+  data = YAML.load_file(path)
+  data.each do |key, value|
+    library[key] = {}
+    library[key]["english"] = value[0]
+    library[key]["japanese"] = value[1]
+  end
 end
+
+load_library("/lib/emoticons.yml")
 
 def get_japanese_emoticon
   # code goes here
